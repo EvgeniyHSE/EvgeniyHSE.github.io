31ab008cdd8942eaf746113e5044d9972fe0792e
Evgeniy Urnyshev <uevgenii@yandex.ru>
2018-08-17 18:24:41 +0300

    Submit button style changed while sending message

--- assets/js/submittosheets.js
+++ assets/js/submittosheets.js
@@ -1,5 +1,6 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwYblgZevI_2Ck5eTJsATe1YG03KcWt_J5KszLyaPLRKf4jUPmU/exec'
 const form = document.forms['submit-to-google-sheet']
+const $button = $("input:submit")
 
 form.addEventListener('submit', e => {
 	e.preventDefault()
@@ -8,15 +9,18 @@ form.addEventListener('submit', e => {
 		alert('Введите имя и сообщение')
 		return
 	}
+	$button.addClass('sending')
 
 	fetch(scriptURL, { method: 'POST', body: new FormData(form)})
 	  .then(response => {
 				console.log('Success!', response)
 				form.name.value = form.subject.value = form.message.value = ''
+				$button.removeClass('sending')
 				alert('Сообщение отправлено.')
 				})
 	  .catch(error => {
-				console.error('Error!', error.message) 
+				console.error('Error!', error.message)
+				$button.removeClass('sending')
 				window.alert('Ошибка при отправке.')
 				})
 })
